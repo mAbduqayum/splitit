f1affc03aefc0cb3a19247dcc39d852e59ec7c1a
mAbduqayum <mabduqayum@gmail.com>
2025-08-13 21:55:32 +0500

    new: add tailwind css

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
+      <h1 class="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+      <router-outlet />
   `,
   styles: [],
 })
